b998f2c740cc4ebb7172600beb6547412ac223c4
Timofey <nsk.timka2077@gmail.com>
2024-01-24 22:08:49 +0200

    some changes

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -96,7 +96,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'backend_static/static'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
