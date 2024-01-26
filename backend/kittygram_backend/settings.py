6f356ba31feca9933973c7596ec9d43a6a1f25c3
Timofey <nsk.timka2077@gmail.com>
2024-01-26 18:02:51 +0200

    some changes in settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(";")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost;127.0.0.1").split(";")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
