14198c72d45144f8f336bbbf2b98540e2d6a729a
Timofey <nsk.timka2077@gmail.com>
2024-01-26 19:02:15 +0200

    some changes in settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "key")
 
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
