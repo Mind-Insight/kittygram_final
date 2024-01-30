1a42ea0295498ce88adcc884a9b5f80f2915701d
Timofey <nsk.timka2077@gmail.com>
2024-01-30 22:16:02 +0200

    some changes

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "key")
 
-DEBUG = os.getenv("DEBUG", False) == True
+DEBUG = True
 
 ALLOWED_HOSTS = ["kittyygrammm.ddns.net"]
 
