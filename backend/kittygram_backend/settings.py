053ac26d743782b0562975debe95f364b65c9a07
Timofey <nsk.timka2077@gmail.com>
2024-01-30 21:43:18 +0200

    experimental changes

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "key")
 
-DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost;127.0.0.1").split(";")
 
